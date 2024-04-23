ec23850583177c534e57597618f6dd151a939369
Kristina <lumosmaxima950@gmail.com>
2024-04-24 00:44:35 +0300

    try1

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,23 @@
 import {encoded, translations} from './data.js'
 
 console.log("Let's rock")
-console.log(encoded, translations)
+//console.log(encoded, translations)
 
+const uniqIds = new Set();
 
+function decodeFields(encoded, translations){
+    for (let key in encoded[i]){
+        if (key.endsWith('Id') && !='groupId' && !='service' && !='formatSize' && !='ca'){
+            for (let key in translations){
+                if (key.includes(encoded[i].[key])){
+                    encoded[i].[key] = [key];
+                    uniqIds.add(encoded[i].key);
+                }
+            }
+        }
+    }
+}
 
-// console.log(decoded)
+const decoded = new Map(encoded);
+
+console.log(decoded)
